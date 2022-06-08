1e323c9d1ff4e48d8e7fe55b8a0611d8468012ea
Mcoy Paco <marcopaco1230@gmail.com>
2022-06-08 11:06:32 +0000

    Initial Vue Tailwind integration

---
+++ resources/js/Pages/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h1 class="text-3xl font-bold">
+      Home Page
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted () {
+    console.log('Component mounted.')
+  }
+}
+</script>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        @yield('content')
+    </div>
+
+    <!-- Scripts -->
+    <script src="/js/manifest.js"></script>
+    <script src="/js/vendor.js"></script>
+    <script src="/js/app.js"></script>
+</body>
+</html>
