14904548f1820f9f78e5fc132be233066afa6c1c
Mcoy Paco <marcopaco1230@gmail.com>
2022-06-12 06:11:06 +0000

    Optimizations

--- resources/js/Components/ManageLocationsModal.vue
+++ resources/js/Components/ManageLocationsModal.vue
@@ -1,36 +1,15 @@
 <template>
-  <!-- This example requires Tailwind CSS v2.0+ -->
   <div
     class="relative z-10"
     aria-labelledby="modal-title"
     role="dialog"
     aria-modal="true"
   >
-    <!--
-    Background backdrop, show/hide based on modal state.
-
-    Entering: "ease-out duration-300"
-      From: "opacity-0"
-      To: "opacity-100"
-    Leaving: "ease-in duration-200"
-      From: "opacity-100"
-      To: "opacity-0"
-  -->
-    <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+    <div class="fixed inset-0 bg-gray-500 bg-opacity-75" />
 
     <div class="fixed z-10 inset-0 overflow-y-auto">
       <div class="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
-        <!--
-        Modal panel, show/hide based on modal state.
-
-        Entering: "ease-out duration-300"
-          From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-          To: "opacity-100 translate-y-0 sm:scale-100"
-        Leaving: "ease-in duration-200"
-          From: "opacity-100 translate-y-0 sm:scale-100"
-          To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-      -->
-        <div class="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full">
+        <div class="relative bg-white rounded-lg text-left overflow-hidden shadow-xl sm:my-8 sm:max-w-lg sm:w-full">
           <ManageLocationsForm />
         </div>
       </div>
